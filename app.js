95ab2e954fb1a54e80159fb6236abb998993ff45
Wilson Castillo <wilxoncastillo@gmail.com>
2019-04-22 18:38:43 -0400

    19-. Separación de datos entre el modelo y la vista

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ Vue.component('app-icon', {
 })
 
 Vue.component('app-task', {
+    data: function() {
+        return {
+            editing: false,
+            draft: '',
+        };
+    },
     template: '#task-template',
     props: ['tasks', 'task', 'index'],
     methods:{
@@ -17,26 +23,28 @@ Vue.component('app-task', {
         },
 
         edit: function(){
+            /*
             this.tasks.forEach(function(task){
                 task.editing = false;
             });
+            */
 
             this.draft = this.task.description;
 
-            this.task.editing = true;
+            this.editing = true;
         },
 
         update: function(){
-            this.task.description = this.draft;
-            this.task.editing = false;
+            this.description = this.draft;
+            this.editing = false;
         },
 
         discard: function(){
-            this.task.editing = false;
+            this.editing = false;
         },
         
         remove: function(){
-            this.tasks.splice(this.index, 1);
+            this.splice(this.index, 1);
         }
     }
 });
@@ -45,22 +53,18 @@ var vm = new Vue({
     el: '#app',
     data: {
         new_task: '',
-        draft: '',
         tasks: [
             {
                 description: 'Aprender Vue.js',
                 pending: true,
-                editing: false
             },
             {
                 description: 'Suscribirse a Styde.net',
                 pending: true,
-                editing: false
             },
             {
                 description: 'Crear una API',
                 pending: false,
-                editing: false
             }
         ]
     },
@@ -81,81 +85,3 @@ var vm = new Vue({
         }
     }
 });
-
-
-/*
-var vm = new Vue ({
-	el : "#app",
-	
-	methods: {
-		createTask: function () {
-	    	this.tasks.push({
-	        description: this.new_task,
-	        pending: true,
-	        editing: false
-	    });
-	 
-	    this.new_task = '';
-		},
-		
-		toogleStatus: function (task) {
-			task.pending = !task.pending;
-		},
-
-		editTask: function (task) {
-			this.tasks.forEach(function (task){
-				task.editing = false;				;
-			});
-
-			this.draft = task.description;
-
-			task.editing = true;
-		},
-
-		updateTask: function (task) {
-			task.description = this.draft;
-
-			task.editing = false;
-		},
-
-		discardTask: function (task) {
-			task.editing = false;
-		},
-
-		deleteTask: function (index) {
-			this.tasks.splice(index, 1);
-		},
-
-		deleteCompleted: function () {
-			this.tasks = this.tasks.filter(function (task) {
-				return task.pending
-			});
-		},
-
-		
-	},
-
-	data: {
-		new_task: '',
-		draft: '',
-		tasks: [
-		{
-			description: 'Aprender Vue.js',
-			pending: true,
-			editing: false
-		},
-		{
-			description: 'Sucribirse a Styde.net',
-			pending: true,
-			editing: false
-		},
-		{
-			description: 'Grabar lección de Vue',
-			pending: false,
-			editing: false
-		}
-		]
-	}
-});
-
-*/
